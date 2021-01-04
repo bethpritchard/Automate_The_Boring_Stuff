2b38ff7fed00bd95f1908426232dbeb479db06fc
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-01-04 21:40:31 +0000

    added addToInventory function

--- AutomateTheBoringStuff/CH5/fantasyGameInventory.py
+++ AutomateTheBoringStuff/CH5/fantasyGameInventory.py
@@ -1,6 +1,11 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+"""
+Fantasy Game Inventory
+"""
+
+#stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 def displayInventory(inventory):
+    """Prints contents and total number of items"""
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
@@ -8,4 +13,22 @@ def displayInventory(inventory):
         item_total += v
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+
+"""
+Dictionary to list 
+"""
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def addToInventory(inventory, addedItems):
+    """ Adds items to dictionary """
+
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
