eb985792b3331dc50b89670b994620defa197305
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-01-05 19:02:40 +0000

    created file

--- AutomateTheBoringStuff/CH6/table.py
+++ AutomateTheBoringStuff/CH6/table.py
@@ -8,6 +8,7 @@ def printTable(data):
 
 
 
+
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
                  ['Alice', 'Bob', 'Carol', 'David'],
                  ['dogs', 'cats', 'moose', 'goose']]
