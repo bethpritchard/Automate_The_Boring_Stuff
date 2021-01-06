5519ab1e0470336f5707083b3b465748e487ed7c
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-01-06 12:38:42 +0000

    printTable

--- AutomateTheBoringStuff/CH6/table.py
+++ AutomateTheBoringStuff/CH6/table.py
@@ -1,14 +1,25 @@
 def printTable(data):
     colWidths = [0] *len(data)
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            maxlength = len(data[i][j])
-            print(data[i][j])
-            print(len(data[i][j]))
-        #maxlength = max(len(data[i]))
-        #print(max)
 
-    print(colWidths)
+    #find column widths and sets colWidths[i] to width
+    for j in range(len(data)):
+        lst = data[j]
+        lengths = []
+        for i in range(len(lst)):
+            lengths.append(len(lst[i]))
+        maxlength = max(lengths)
+        colWidths[j] = maxlength
+
+    #prints using the method in CH4 - 'flipping' the table
+    #For future ref - it prints str
+
+
+    for x in range(len(data[0])) :
+        for y in range(len(data)) :
+            print(data[y][x].ljust(colWidths[y]), end = '   ')
+        print()
+
+
 
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
                  ['Alice', 'Bob', 'Carol', 'David'],
